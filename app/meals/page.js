e4e3322f0ca1b5666cd12b0d323d85478df6716c
Kekto <lekto8814@gmail.com>
2025-07-01 21:32:14 +0200

    Implement meals loading functionality with suspense and loading state

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,11 +1,19 @@
 import Link from "next/link";
 import classes from "./page.module.css";
 import MealsGrid from "@/components/meals/meals-grid";
+import { getMeals } from "@/lib/meals";
+import { Suspense } from "react";
+
+async function Meals() {
+	const meals = await getMeals();
+
+	return <MealsGrid meals={meals} />;
+}
 
 export default function MealsPage() {
 	return (
 		<>
-			<header>
+			<header className={classes.header}>
 				<h1>
 					Delicious Meals, created{" "}
 					<span className={classes.highlight}>by you</span>
@@ -18,7 +26,10 @@ export default function MealsPage() {
 				</p>
 			</header>
 			<main className={classes.main}>
-				<MealsGrid meals={[]} />
+				<Suspense
+					fallback={<p className={classes.loading}>Loading meals...</p>}>
+					<Meals />
+				</Suspense>
 			</main>
 		</>
 	);
